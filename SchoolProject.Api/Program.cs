d1a39329b89e68ed063c9e2129539794fb5193fe
khaled979797 <khaled.alshreef25@gmail.com>
2024-09-28 07:52:43 +0300

    Adding Filter Based On Roles

--- SchoolProject.Api/Program.cs
+++ SchoolProject.Api/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using SchoolProject.Core;
+using SchoolProject.Core.Filters;
 using SchoolProject.Core.Middleware;
 using SchoolProject.Data.Entities.Identity;
 using SchoolProject.Infrastructure;
@@ -80,6 +81,8 @@ namespace SchoolProject.Api
                 var factory = x.GetRequiredService<IUrlHelperFactory>();
                 return factory.GetUrlHelper(actionContext);
             });
+
+            builder.Services.AddTransient<AuthFilter>();
 
             var app = builder.Build();
 
